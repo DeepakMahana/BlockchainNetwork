aeed2fa1b50a93e6b674b396aa24be460d9fab61
Dcode10 <mahana.deepak20@gmail.com>
2020-05-09 22:04:34 +0530

    Gitignore edited

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -44244,7 +44244,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42799" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40395" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
